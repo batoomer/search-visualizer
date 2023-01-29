89a14434e1c1a4e29ce8ba29a336bcb84ea4b275
Batouchan Omer <batouchan.omer@gmail.com>
2023-01-29 15:56:47 +0200

    Added Home Page

---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,57 @@
+import Page from "../../base/Page";
+import Article from "../../components/article/Article";
+import homeArticleData from './home.json';
+import "./home.css";
+
+export default class HomePage extends Page{
+    /**
+     * @constructor
+     */
+    constructor(){
+        super('homepage');
+    };
+
+    /**
+     * Creates the content of the home page.
+     *
+     * @property {function} create
+     * @returns {HTMLElement} - The component element
+     */
+    create(){
+        this.componentElement.append(
+            this.#createHeroSection(),
+            new Article(homeArticleData).create(),
+        );
+        return this.componentElement;
+    };
+
+    /**
+     * Clears any resources used by the home page
+     *
+     * @property {function} clearResources
+     */
+    clearResources(){
+    };
+
+    /**
+     * Creates the hero section for the Home Page
+     *
+     * @property {function} createHeroSection
+     * @private
+     * @returns {HTMLElement} - The hero section element
+     */
+    #createHeroSection(){
+        const container = document.createElement('div');
+        container.classList.add('container');
+
+        const heroSection = document.createElement('section');
+        heroSection.classList.add('home__hero-section');
+        heroSection.innerHTML = `
+            <h1>Search Techniques In Artificial Intelligence</h1>
+        `;
+
+        container.appendChild(heroSection);
+        return container;
+    };
+}
+
